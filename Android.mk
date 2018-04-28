488ef79c6fdf347de1fd9e609a607d1b07fc91e1
v0idifier <duckifier@protonmail.ch>
2018-04-28 01:03:18 +0000

    Makefile: bypass some more guards for precaution

--- Android.mk
+++ Android.mk
@@ -3,20 +3,20 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
-ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
+#ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
-endif
+#endif
 
-ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
+#ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 
-ifeq ($(BOARD_USES_ADRENO), true)
+#ifeq ($(BOARD_USES_ADRENO), true)
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
-endif
+#endif
 
 include $(QCOM_MEDIA_ROOT)/hypv-intercept/Android.mk
 
-endif
+#endif
 
 #endif
