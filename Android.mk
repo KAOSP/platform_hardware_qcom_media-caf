0b738643dd76b3850eb44bbc879176eb3d9d6289
Pedro Kalil <kalilgamer0@gmail.com>
2018-05-13 00:53:13 +0000

    makefile: Introduce a new way to select the hal to be used without relying on pathmaps

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-#ifeq ($(call my-dir),$(call project-path-for,qcom-media))
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-msm8996)
 
 QCOM_MEDIA_ROOT := $(call my-dir)
 
@@ -19,4 +19,4 @@ include $(QCOM_MEDIA_ROOT)/hypv-intercept/Android.mk
 
 #endif
 
-#endif
+endif
